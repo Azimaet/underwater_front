3e6a0d1a4126c7af63edc3db95b661f15ebee038
azimaet <maximedolo@gmail.com>
2022-10-24 07:04:14 +0200

    fix. createUser gql mutation response body

--- src/graphql/mutations/createUser.ts
+++ src/graphql/mutations/createUser.ts
@@ -3,7 +3,9 @@ import gql from "graphql-tag";
 export const MUTATION_CREATE_USER = gql`
   mutation createUser($input: createUserInput!) {
     createUser(input: $input) {
-      dive
+      user {
+        id
+      }
     }
   }
 `;
