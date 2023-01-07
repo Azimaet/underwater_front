cabed4d858ad3eb04411db3652dc30c6a11b251e
azimaet <maximedolo@gmail.com>
2023-01-07 17:55:36 +0100

    refac. rwd behaviour on dive list pages

--- src/components/organisms/DivesList.vue
+++ src/components/organisms/DivesList.vue
@@ -41,18 +41,24 @@ const divesCollection = await useGqlQueryManager(GraphqlActions.DIVES, {
         rounded
       >
         <v-card-text>
-          <v-list density="compact">
-            <v-list-subheader>DIVES</v-list-subheader>
-
+          <v-list :tag="'ul'" density="compact">
             <v-list-item
               v-for="(dive, index) in divesCollection"
               :key="index"
               :value="dive"
               active-color="primary"
               :lines="'one'"
+              :tag="'li'"
             >
-              <v-list-item-title>
-                {{ "#" + (divesCollection.length - index) }}
+              <v-list-item-title
+                :class="['d-flex', 'flex-row', 'justify-space-between']"
+              >
+                <span>
+                  {{ "#" + (divesCollection.length - index) }}
+                </span>
+                <span>
+                  {{ format(new Date(dive.date), "PP") }}
+                </span>
               </v-list-item-title>
               <v-list-item-subtitle
                 :class="[
@@ -61,62 +67,130 @@ const divesCollection = await useGqlQueryManager(GraphqlActions.DIVES, {
                   'align-self-center',
                 ]"
               >
-                <v-row>
-                  <v-col :cols="1">
-                    {{ format(new Date(dive.date), "PP") }}
-                  </v-col>
-                  <v-col :cols="1">
-                    <v-icon icon="mdi-clock-outline" />
-                    {{ dive.totalTime }}mn
-                  </v-col>
-                  <v-col :cols="1">
-                    <v-icon icon="mdi-chart-bell-curve-cumulative" />
-                    {{ dive.maxDepth }}mt
-                  </v-col>
-                  <v-col :cols="1">
-                    <v-icon icon="mdi-gas-cylinder" />
-                    {{ useGasNameProvider(dive.gasTanks[0].gasMix).title }}
-                  </v-col>
-                  <v-col :cols="6">
-                    <v-chip
-                      size="small"
-                      :variant="'outlined'"
-                      :color="Colors['theme_' + dive.divingEnvironment?.token.replaceAll('%', '') as keyof typeof Colors]"
-                    >
-                      {{ dive.divingEnvironment?.label }}
-                    </v-chip>
-                    <v-chip
-                      size="small"
-                      :color="Colors['theme_' + dive.divingRole?.token.replaceAll('%', '') as keyof typeof Colors]"
-                      :variant="'text'"
-                    >
-                      {{ dive.divingRole?.label }}
-                    </v-chip>
-                    <v-chip
-                      v-for="theme in dive.divingType.edges"
-                      :key="theme"
-                      size="small"
-                      :color="Colors['theme_' + theme.node.token.replaceAll('%', '') as keyof typeof Colors]"
-                      :variant="'tonal'"
-                    >
-                      {{ theme.node.label }}
-                    </v-chip>
-                  </v-col>
-                  <v-col :cols="2">
-                    <ButtonComponent
-                      :label="EDIT"
-                      :color="'warning'"
-                      :responsive="true"
-                      @click="
-                        $router.push({
-                          name: 'dive_form',
-                          state: { dive: dive },
-                        })
-                      "
-                    />
-                    <FormDiveDeleteModal :id="dive.id" />
-                  </v-col>
-                </v-row>
+                <v-container :class="['d-none', 'd-md-flex']">
+                  <v-row>
+                    <v-col :cols="1">
+                      <v-icon icon="mdi-clock-outline" />
+                      {{ dive.totalTime }}mn
+                    </v-col>
+                    <v-col :cols="1">
+                      <v-icon icon="mdi-chart-bell-curve-cumulative" />
+                      {{ dive.maxDepth }}mt
+                    </v-col>
+                    <v-col :cols="1">
+                      <v-icon icon="mdi-gas-cylinder" />
+                      {{ useGasNameProvider(dive.gasTanks[0].gasMix).title }}
+                    </v-col>
+                    <v-col :cols="7">
+                      <v-chip
+                        size="small"
+                        :variant="'outlined'"
+                        :color="Colors['theme_' + dive.divingEnvironment?.token.replaceAll('%', '') as keyof typeof Colors]"
+                      >
+                        {{ dive.divingEnvironment?.label }}
+                      </v-chip>
+                      <v-chip
+                        size="small"
+                        :color="Colors['theme_' + dive.divingRole?.token.replaceAll('%', '') as keyof typeof Colors]"
+                        :variant="'text'"
+                      >
+                        {{ dive.divingRole?.label }}
+                      </v-chip>
+                      <v-chip
+                        v-for="theme in dive.divingType.edges"
+                        :key="theme"
+                        size="small"
+                        :color="Colors['theme_' + theme.node.token.replaceAll('%', '') as keyof typeof Colors]"
+                        :variant="'tonal'"
+                      >
+                        {{ theme.node.label }}
+                      </v-chip>
+                    </v-col>
+                    <v-col :cols="2">
+                      <ButtonComponent
+                        :label="EDIT"
+                        :color="'warning'"
+                        :responsive="true"
+                        @click="
+                          $router.push({
+                            name: 'dive_form',
+                            state: { dive: dive },
+                          })
+                        "
+                      />
+                      <FormDiveDeleteModal :id="dive.id" />
+                    </v-col>
+                  </v-row>
+                </v-container>
+
+                <v-container :class="['d-md-none']">
+                  <v-row>
+                    <v-col :cols="9">
+                      <div>
+                        <v-row>
+                          <v-col :class="['d-flex', 'flex-row']">
+                            <div :class="['mr-4']">
+                              <v-icon icon="mdi-clock-outline" />
+                              {{ dive.totalTime }}mn
+                            </div>
+                            <div :class="['mr-4']">
+                              <v-icon icon="mdi-chart-bell-curve-cumulative" />
+                              {{ dive.maxDepth }}mt
+                            </div>
+                            <div :class="['mr-4']">
+                              <v-icon icon="mdi-gas-cylinder" />
+                              {{
+                                useGasNameProvider(dive.gasTanks[0].gasMix)
+                                  .title
+                              }}
+                            </div>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col>
+                            <v-chip
+                              size="small"
+                              :variant="'outlined'"
+                              :color="Colors['theme_' + dive.divingEnvironment?.token.replaceAll('%', '') as keyof typeof Colors]"
+                            >
+                              {{ dive.divingEnvironment?.label }}
+                            </v-chip>
+                            <v-chip
+                              size="small"
+                              :color="Colors['theme_' + dive.divingRole?.token.replaceAll('%', '') as keyof typeof Colors]"
+                              :variant="'text'"
+                            >
+                              {{ dive.divingRole?.label }}
+                            </v-chip>
+                            <v-chip
+                              v-for="theme in dive.divingType.edges"
+                              :key="theme"
+                              size="small"
+                              :color="Colors['theme_' + theme.node.token.replaceAll('%', '') as keyof typeof Colors]"
+                              :variant="'tonal'"
+                            >
+                              {{ theme.node.label }}
+                            </v-chip>
+                          </v-col>
+                        </v-row>
+                      </div>
+                    </v-col>
+                    <v-col :cols="3">
+                      <ButtonComponent
+                        :label="EDIT"
+                        :color="'warning'"
+                        :responsive="true"
+                        @click="
+                          $router.push({
+                            name: 'dive_form',
+                            state: { dive: dive },
+                          })
+                        "
+                      />
+                      <FormDiveDeleteModal :id="dive.id" />
+                    </v-col>
+                  </v-row>
+                </v-container>
               </v-list-item-subtitle>
             </v-list-item>
           </v-list>
